7b75d64c5c33288ffe740f8e492bd6c3a4c3649f
Nicolas MEO <nicolas.meo@gmail.com>
2019-10-29 14:09:31 +0100

    Correction affichage bourse

--- buchettes_app/templatetags/get_item.py
+++ buchettes_app/templatetags/get_item.py
@@ -5,6 +5,7 @@ from django.template.defaulttags import register
 def get_item(dictionary, key):
     return dictionary.get(key)
 
+
 @register.filter
 def get_item_count(dictionary, key):
     return dictionary.get(key).count()
